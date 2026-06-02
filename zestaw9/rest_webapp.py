e0f391d7017336269dc3d6a3a88e7adad0743dfe
JF <kvbv.figura@gmail.com>
2026-06-02 22:40:45 +0200

    modyfikacje serwera REST, skrypt testujacy, baza danych

--- zestaw9/rest_webapp.py
+++ zestaw9/rest_webapp.py
@@ -16,13 +16,12 @@ plik_bazy = './osoby.sqlite'
 import re, sqlite3
 from urllib.parse import parse_qs
 
-class OsobyApp:
+class RestApp:
+    table = None   # nazwa tabeli SQL, ustawiana w klasach pochodnych
+    path  = None   # bazowa ścieżka URL '/osoby' lub '/psy'
+    search_fields = []  # pola dozwolone w /search, ustawiane w klasach pochodnych
+
     def __init__(self, environment, start_response):
-        '''
-        Konstruktor wywoływany przez serwer WSGI. Jak każdy konstruktor tworzy nowy
-        obiekt, następnie zapamiętuje w jego polach przekazane przez serwer argumenty
-        i inicjuje pola na odpowiedź.
-        '''
         self.env = environment
         self.start_response = start_response
         self.status = '200 OK'
@@ -30,11 +29,6 @@ class OsobyApp:
         self.content = b''
 
     def __iter__(self):
-        '''
-        Metoda obsługująca proces iterowania po stworzonym obiekcie. Serwer WSGI
-        wymaga aby w środku była co najmniej jedna instrukcja "yield" zwracająca
-        ciąg bajtów do odesłania klientowi HTTP.
-        '''
         try:
             self.route()
         except sqlite3.Error as e:
@@ -46,10 +40,6 @@ class OsobyApp:
         yield self.content
 
     def failure(self, status, detail = None):
-        '''
-        Metoda wstawiająca do pól obiektu status błędu oraz dokument HTML
-        z komunikatem o jego wystąpieniu.
-        '''
         self.status = status
         s = '<html>\n<head>\n<title>' + status + '</title>\n</head>\n'
         s += '<body>\n<h1>' + status + '</h1>\n'
@@ -59,39 +49,26 @@ class OsobyApp:
         self.content = s.encode('UTF-8')
 
     def route(self):
-        '''
-        Pierwszą rzeczą, którą aplikacja musi zrobić po odebraniu zapytania, jest
-        sprawdzenie nazwy metody HTTP oraz nazwy zasobu. Jest to konieczne aby się
-        zorientować o co klient prosi i wywołać odpowiedni fragment kodu realizujący
-        jego zlecenie. Jest to tzw. routing zapytania.
-
-        W niniejszej aplikacji routing jest realizowany częściowo w tej metodzie,
-        a częściowo w metodach handle_table() i handle_item().
-        '''
-        if self.env['PATH_INFO'] == '/osoby':
+        if self.env['PATH_INFO'] == self.path:
             self.handle_table()
             return
-        if self.env['PATH_INFO'] == '/osoby/search':
+        if self.env['PATH_INFO'] == self.path + '/search':
             self.handle_search()
             return
-        m = re.search('^/osoby/(?P<id>[0-9]+)$', self.env['PATH_INFO'])
+        m = re.search('^' + self.path + '/(?P<id>[0-9]+)$', self.env['PATH_INFO'])
         if m is not None:
             self.handle_item(m.group('id'))
             return
         self.failure('404 Not Found')
 
     def handle_table(self):
-        '''
-        Obsługa zapytań odnoszących się do tabeli "osoby" traktowanej jako całość.
-        Można ją pobrać, albo można dodać do niej nowy wiersz.
-        '''
         if self.env['REQUEST_METHOD'] == 'GET':
             colnames, rows = self.sql_select()
             self.send_rows(colnames, rows)
         elif self.env['REQUEST_METHOD'] == 'POST':
             colnames, vals = self.read_tsv()
-            q = 'INSERT INTO osoby (' + ', '.join(colnames) + ') VALUES ('
-            q += ', '.join(['?' for v in vals]) + ')'
+            q = 'INSERT INTO ' + self.table + ' (' + ', '.join(colnames) + ') VALUES ('
+            q += ', '.join(['?' for _ in vals]) + ')'
             id = self.sql_modify(q, vals)
             colnames, rows = self.sql_select(id)
             self.send_rows(colnames, rows)
@@ -99,10 +76,6 @@ class OsobyApp:
             self.failure('501 Not Implemented')
 
     def handle_item(self, id):
-        '''
-        Obsługa zapytań odnoszących się do konkretnego wiersza w tabeli "osoby".
-        Można go pobrać, zmodyfikować, albo usunąć.
-        '''
         if self.env['REQUEST_METHOD'] == 'GET':
             colnames, rows = self.sql_select(id)
             if len(rows) == 0:
@@ -111,22 +84,19 @@ class OsobyApp:
                 self.send_rows(colnames, rows)
         elif self.env['REQUEST_METHOD'] == 'PUT':
             colnames, vals = self.read_tsv()
-            q = 'UPDATE osoby SET '
+            q = 'UPDATE ' + self.table + ' SET '
             q += ', '.join([c + ' = ?' for c in colnames])
             q += ' WHERE id = ' + str(id)
             self.sql_modify(q, vals)
             colnames, rows = self.sql_select(id)
             self.send_rows(colnames, rows)
         elif self.env['REQUEST_METHOD'] == 'DELETE':
-            q = 'DELETE FROM osoby WHERE id = ' + str(id)
+            q = 'DELETE FROM ' + self.table + ' WHERE id = ' + str(id)
             self.sql_modify(q)
         else:
             self.failure('501 Not Implemented')
-    
-    def handle_search(self):
-        ## metoda do parsowania zapytania SQL
-        self.env['QUERY_STRING']
 
+    def handle_search(self):
         if self.env['REQUEST_METHOD'] != 'GET':
             self.failure('501 Not Implemented')
             return
@@ -134,21 +104,19 @@ class OsobyApp:
 
         conditions = []
         vals = []
-
-        if 'imie' in params:
-            conditions.append('imie = ?')
-            vals.append(params['imie'][0])
-        if 'nazwisko' in params:
-            conditions.append('nazwisko = ?')
-            vals.append(params['nazwisko'][0])
+        for field in self.search_fields:
+            if field in params:
+                conditions.append(field + ' = ?')
+                vals.append(params[field][0])
 
         if not conditions:
-            self.failure('400 Bad Request', 'Podaj parametr imie i/lub nazwisko')
+            self.failure('400 Bad Request',
+                'Podaj parametr: ' + ' i/lub '.join(self.search_fields))
             return
 
         conn = sqlite3.connect(plik_bazy)
         crsr = conn.cursor()
-        query = 'SELECT * FROM osoby WHERE ' + ' AND '.join(conditions)
+        query = 'SELECT * FROM ' + self.table + ' WHERE ' + ' AND '.join(conditions)
         crsr.execute(query, vals)
         colnames = [d[0] for d in crsr.description]
         rows = crsr.fetchall()
@@ -156,8 +124,6 @@ class OsobyApp:
         conn.close()
         self.send_rows(colnames, rows)
 
-        
-        
     def read_tsv(self):
         f = self.env['wsgi.input']
         n = int(self.env['CONTENT_LENGTH'])
@@ -178,7 +144,7 @@ class OsobyApp:
     def sql_select(self, id = None):
         conn = sqlite3.connect(plik_bazy)
         crsr = conn.cursor()
-        query = 'SELECT * FROM osoby'
+        query = 'SELECT * FROM ' + self.table
         if id is not None:
             query += ' WHERE id = ' + str(id)
         crsr.execute(query)
@@ -191,19 +157,41 @@ class OsobyApp:
     def sql_modify(self, query, params = None):
         conn = sqlite3.connect(plik_bazy)
         crsr = conn.cursor()
+        # pragma foreing_keys 
+        crsr.execute('PRAGMA foreign_keys = ON')
         if params is None:
             crsr.execute(query)
         else:
             crsr.execute(query, params)
-        rowid = crsr.lastrowid   # id wiersza wstawionego przez INSERT
+        rowid = crsr.lastrowid
         crsr.close()
         conn.commit()
         conn.close()
         return rowid
 
+
+class OsobyApp(RestApp):
+    table = 'osoby'
+    path  = '/osoby'
+    search_fields = ['imie', 'nazwisko']
+
+
+class PsyApp(RestApp):
+    table = 'psy'
+    path  = '/psy'
+    search_fields = ['imie', 'rasa']
+
+# object_router pozwala przekazać odpowiedni obiekt do klasy RestAPP
+def object_router(environment, start_response):
+    path = environment.get('PATH_INFO', '')
+    if path.startswith('/psy'):
+        return PsyApp(environment, start_response)
+    return OsobyApp(environment, start_response)
+
+
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
     port = 8000
-    httpd = make_server('', port, OsobyApp)
+    httpd = make_server('', port, object_router)
     print('Listening on port %i, press ^C to stop.' % port)
     httpd.serve_forever()
